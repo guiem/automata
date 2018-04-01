6f3cb4ca398db60059aa30e992a36f8cca665a1e
guiem <g@guiem.info>
2018-04-01 15:31:48 +0100

    added loop label treatement

--- automata/fa/fa.py
+++ automata/fa/fa.py
@@ -5,6 +5,7 @@ import abc
 from automata.shared.automaton import Automaton
 import networkx as nx
 from nxpd import draw
+from collections import defaultdict
 
 class FA(Automaton, metaclass=abc.ABCMeta):
     """An abstract base class for finite automata."""
@@ -18,8 +19,14 @@ class FA(Automaton, metaclass=abc.ABCMeta):
         G.add_nodes_from(["start"], shape='point', color='gray')
         G.add_edges_from([("start", self.initial_state, {'color': 'gray'})])
         edges = []
+        loops = defaultdict(set)
         for o, trans in self.transitions.items():
             for symbol, d in trans.items():
-                edges.append((o, d, {'label': symbol}))
+                if o == d:
+                    loops[o].add(symbol)
+                else:
+                    edges.append((o, d, {'label': symbol}))
+        for o, symbols in loops.items():
+            edges.append((o, o, {'label': '\, '.join(sorted(symbols))}))
         G.add_edges_from(edges)
         return draw(G, show=show)
